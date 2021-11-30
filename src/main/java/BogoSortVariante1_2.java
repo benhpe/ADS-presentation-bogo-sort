ae38b99c7ad5b632ed04eae7af41540f1fad49c4
benjo0001 <ben.hoppe6.0@gmail.com>
2021-11-30 08:26:42 +0100

    refactor BogoSortVariante1_2

--- src/main/java/BogoSortVariante1_2.java
+++ src/main/java/BogoSortVariante1_2.java
@@ -1,24 +1,17 @@
-public class BogoSort {
-    // Sorts array a[0..n-1] using Bogo sort
+public class BogoSortVariante1_2 {
     void bogoSort(int[] a)
     {
-        // if array is not sorted then shuffle the
-        // array again
-        while (isSorted(a) == false)
+        while (!isSorted(a)){
             shuffle(a);
+        }
     }
-
-    // To generate permutation of the array
     void shuffle(int[] a)
     {
-        // Math.random() returns a double positive
-        // value, greater than or equal to 0.0 and
-        // less than 1.0.
-        for (int i = 0; i < a.length; i++)
+        for (int i = 0; i < a.length; i++){
             swap(a, i, (int)(Math.random() * i));
+        }
     }
 
-    // Swapping 2 elements
     void swap(int[] a, int i, int j)
     {
         int temp = a[i];
@@ -26,7 +19,6 @@ public class BogoSort {
         a[j] = temp;
     }
 
-    // To check if array is sorted or not
     boolean isSorted(int[] a)
     {
         for (int i = 1; i < a.length; i++)
@@ -35,22 +27,19 @@ public class BogoSort {
         return true;
     }
 
-    // Prints the array
     void printArray(int[] arr)
     {
-        for (int i = 0; i < arr.length; i++)
+        for (int i = 0; i < arr.length; i++){
             System.out.print(arr[i] + " ");
+        }
         System.out.println();
     }
 
     public static void main(String[] args)
     {
-        // Enter array to be sorted here
         int[] a = { 3, 2, 5, 1, 0, 4 };
-        BogoSort ob = new BogoSort();
-
+        BogoSortVariante1_2 ob = new BogoSortVariante1_2();
         ob.bogoSort(a);
-
         System.out.print("Sorted array: ");
         ob.printArray(a);
     }
